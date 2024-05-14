7e65cf1f12cda165b93266984b35ac7baa2a40c5
Edward Yang <edward@innoshop.com>
2024-05-14 09:49:40 +0800

    Update footer content as default.

--- innopacks/front/resources/views/layouts/footer.blade.php
+++ innopacks/front/resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="footer-box">
   <div class="top-bg"><img src="{{ asset('images/front/home/footer-bg.svg') }}" class="img-fluid"></div>
   <div class="container">
-    <div class="top-title">InnoShop 创新开源电商系统</div>
+    <div class="top-title">InnoCMS 轻量级企业官网建站系统</div>
     <div class="bottom-box">
       <div class="row">
         <div class="col-md-6">
@@ -10,7 +10,7 @@
           </div>
         </div>
         <div class="col-md-6 copyright-text">
-          <a href="https://www.innocms.com" class="ms-2" target="_blank">InnoShop</a>
+          <a href="https://www.innocms.com" class="ms-2" target="_blank">InnoCMS</a>
           &copy; {{ date('Y') }} All Rights Reserved
           @if(system_setting('icp_number'))
             <a href="https://beian.miit.gov.cn" class="ms-2" target="_blank">{{ system_setting('icp_number') }}</a>
