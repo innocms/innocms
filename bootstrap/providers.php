9dc615595e4aa149806e03d67631165665bb55a5
Edward Yang <edward@innoshop.com>
2024-06-28 15:55:02 +0800

    fixed permissions

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\AuthServiceProvider::class,
 
     InnoCMS\Common\CommonServiceProvider::class,
     InnoCMS\Panel\PanelServiceProvider::class,
