96cb31c68520fc9c9def2894f70037fea9848b35
liuwei <3388733314@qq.com>
2024-06-29 11:45:44 +0800

    时间选择器组件

--- innopacks/panel/resources/js/app.js
+++ innopacks/panel/resources/js/app.js
@@ -15,6 +15,19 @@ $(function () {
 
   const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
   const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+  $(document).on('focus', '.date input, .datetime input, .time input', function(event) {
+    if (!$(this).prop('id')) $(this).prop('id', Math.random().toString(36).substring(2));
+
+    $(this).attr('autocomplete', 'off');
+
+    laydate.render({
+      elem: '#' + $(this).prop('id'),
+      type: $(this).parent().hasClass('date') ? 'date' : ($(this).parent().hasClass('datetime') ? 'datetime' : 'time'),
+      trigger: 'click',
+      lang: $('html').prop('lang') == 'zh-cn' ? 'cn' : 'en'
+    });
+  });
 })
 
 const tinymceInit = () => {
