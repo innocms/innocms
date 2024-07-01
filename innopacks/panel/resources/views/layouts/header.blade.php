54dc1d492b7a87d885c8f70fb3cf49f2e7cc22e4
Edward Yang <edward@innoshop.com>
2024-07-01 10:27:25 +0800

    Add locale switch for panel.

--- innopacks/panel/resources/views/layouts/header.blade.php
+++ innopacks/panel/resources/views/layouts/header.blade.php
@@ -1,6 +1,22 @@
 <div class="header-box d-flex justify-content-between align-items-center px-3">
   <div></div>
   <div class="d-flex">
+
+    <div class="header-item dropdown d-flex align-items-center d-none d-lg-flex">
+      <div class="wh-20 me-2"><img src="{{ image_origin('images/flag/'. panel_locale_code().'.png') }}" class="img-fluid"></div>
+      <span class="">{{ current_panel_locale()['name'] }} <i class="bi bi-chevron-down"></i></span>
+      <ul class="dropdown-menu">
+        @foreach (panel_locales() as $locale)
+          <li>
+            <a class="dropdown-item d-flex" href="{{ panel_route('locale.switch', ['code'=> $locale['code']]) }}">
+              <div class="wh-20 me-2"><img src="{{ image_origin($locale['image']) }}" class="img-fluid"></div>
+              {{ $locale['name'] }}
+            </a>
+          </li>
+        @endforeach
+      </ul>
+    </div>
+
     <div class="header-item dropdown d-flex align-items-center">
       <div class="wh-40 rounded-circle overflow-hidden">
         <img src="{{ image_resize() }}" class="img-fluid">
