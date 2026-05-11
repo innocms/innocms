6ec0bef45236b241a67cccc3c52d8926a1bfb20b
Edward <edward@innoshop.com>
2026-05-11 13:37:54 +0800

    update innocms

--- innopacks/panel/resources/views/articles/panes/tab_pane_content.blade.php
+++ innopacks/panel/resources/views/articles/panes/tab_pane_content.blade.php
@@ -1,41 +1,73 @@
 <div class="tab-pane fade mt-3" id="content-tab-pane" role="tabpanel" aria-labelledby="content-tab" tabindex="0">
 
-  <ul class="nav nav-tabs mb-4" id="locales-content-tab" role="tablist">
-    @foreach (locales() as $locale)
-      <li class="nav-item" role="presentation">
-        <button class="nav-link d-flex {{ $loop->first ? 'active' : '' }}" id="locale-{{ $locale->code }}-content-tab"
-          data-bs-toggle="tab" data-bs-target="#locale-{{ $locale->code }}-content-pane" type="button"
-          role="tab" aria-controls="locale-{{ $locale->code }}-content-pane"
-          aria-selected="{{ $loop->first ? 'true' : 'false' }}">
-          <div class="wh-20 me-2">
-            <img src="{{ image_origin($locale->image) }}" class="img-fluid">
-          </div>
-          {{ $locale->name }}
-        </button>
-      </li>
-    @endforeach
-  </ul>
-
-  <div class="tab-content" id="locales-content-tabContent">
-    @foreach (locales() as $locale)
-      @php($localeCode = $locale->code)
-      <div class="tab-pane fade {{ $loop->first ? 'show active' : '' }}"
-        id="locale-{{ $localeCode }}-content-pane" role="tabpanel"
-        aria-labelledby="locale-{{ $localeCode }}-content-tab" tabindex="0">
-
-        <input type="hidden" name="translations[{{ $localeCode }}][locale]" value="{{ $localeCode }}">
-
-        <x-panel-form-rich-text title="{{ __('panel/article.content') }}" name="translations[{{ $localeCode }}][content]"
-                                value="{{ old('translations.' . $localeCode . '.content', $article->translate($localeCode, 'content')) }}"
-                                required
-                                placeholder="{{ __('panel/article.content') }}"/>
-
-        <x-panel-form-image title="{{ __('panel/article.image') }}" name="translations[{{ $localeCode }}][image]"
-                            value="{{ old('translations.' . $localeCode . '.image', $article->translate($localeCode, 'image')) }}"
-                            placeholder="{{ __('panel/article.image') }}"/>
-      </div>
-    @endforeach
+  {{-- 内容 --}}
+  <div class="mb-4">
+    <div class="col-form-label required"><span class="text-danger">* </span>{{ __('panel/article.content') }}</div>
+    <ul class="nav nav-tabs" id="locales-content-tab" role="tablist">
+      @foreach (locales() as $locale)
+        <li class="nav-item" role="presentation">
+          <button class="nav-link d-flex {{ $loop->first ? 'active' : '' }}" id="locale-{{ $locale->code }}-content-tab"
+            data-bs-toggle="tab" data-bs-target="#locale-{{ $locale->code }}-content-pane" type="button"
+            role="tab" aria-controls="locale-{{ $locale->code }}-content-pane"
+            aria-selected="{{ $loop->first ? 'true' : 'false' }}">
+            <div class="wh-20 me-2">
+              <img src="{{ image_origin($locale->image) }}" class="img-fluid">
+            </div>
+            {{ $locale->name }}
+          </button>
+        </li>
+      @endforeach
+    </ul>
+    <div class="tab-content" id="locales-content-tabContent">
+      @foreach (locales() as $locale)
+        @php($localeCode = $locale->code)
+        <div class="tab-pane fade {{ $loop->first ? 'show active' : '' }}"
+          id="locale-{{ $localeCode }}-content-pane" role="tabpanel"
+          aria-labelledby="locale-{{ $localeCode }}-content-tab" tabindex="0">
+          <input type="hidden" name="translations[{{ $localeCode }}][locale]" value="{{ $localeCode }}">
+          <x-panel-form-rich-text title="" name="translations[{{ $localeCode }}][content]"
+                                  value="{{ old('translations.' . $localeCode . '.content', $article->translate($localeCode, 'content')) }}"
+                                  required
+                                  placeholder="{{ __('panel/article.content') }}"/>
+        </div>
+      @endforeach
+    </div>
   </div>
+
+  {{-- 封面图 --}}
+  <div class="mb-3 border-top pt-3">
+    <div class="col-form-label">{{ __('panel/article.image') }}</div>
+    <ul class="nav nav-tabs" id="locales-image-tab" role="tablist">
+      @foreach (locales() as $locale)
+        <li class="nav-item" role="presentation">
+          <button class="nav-link d-flex {{ $loop->first ? 'active' : '' }}" id="locale-{{ $locale->code }}-image-tab"
+            data-bs-toggle="tab" data-bs-target="#locale-{{ $locale->code }}-image-pane" type="button"
+            role="tab" aria-controls="locale-{{ $locale->code }}-image-pane"
+            aria-selected="{{ $loop->first ? 'true' : 'false' }}">
+            <div class="wh-20 me-2">
+              <img src="{{ image_origin($locale->image) }}" class="img-fluid">
+            </div>
+            {{ $locale->name }}
+          </button>
+        </li>
+      @endforeach
+    </ul>
+    <div class="tab-content mt-3" id="locales-image-tabContent">
+      @foreach (locales() as $locale)
+        @php($localeCode = $locale->code)
+        <div class="tab-pane fade {{ $loop->first ? 'show active' : '' }}"
+          id="locale-{{ $localeCode }}-image-pane" role="tabpanel"
+          aria-labelledby="locale-{{ $localeCode }}-image-tab" tabindex="0">
+          <div class="d-flex justify-content-center">
+            <x-panel-form-image title="" name="translations[{{ $localeCode }}][image]"
+                                value="{{ old('translations.' . $localeCode . '.image', $article->translate($localeCode, 'image')) }}"
+                                placeholder="{{ __('panel/article.image') }}"/>
+          </div>
+        </div>
+      @endforeach
+    </div>
+  </div>
+
 </div>
 
 @hookinsert('panel.article.edit.content.bottom')
