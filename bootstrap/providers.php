bbdd31dc74a16f2d3df19b9af91f7e608d5ff459
Edward <edward@innoshop.com>
2026-07-31 17:47:24 +0800

    refactor: move theme boot into FrontServiceProvider, drop ThemeBootProvider
    - innopacks/front: add bootTheme() + loadThemeRoutes() aligned with InnoShop Bundle
    - delete app/Providers/ThemeBootProvider.php
    - bootstrap/providers.php: remove ThemeBootProvider registration

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -13,7 +13,4 @@ return [
     InnoCMS\Front\FrontServiceProvider::class,
     InnoCMS\Install\InstallServiceProvider::class,
     InnoCMS\Plugin\PluginServiceProvider::class,
-
-    // 主题 boot loader(必须放最后):加载 themes/{system.theme}/setup/boot.php。
-    App\Providers\ThemeBootProvider::class,
 ];
