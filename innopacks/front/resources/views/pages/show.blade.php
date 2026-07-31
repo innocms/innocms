e70f3c9b4ba2648e1cc9f18a07cb12997d148221
Edward <edward@innoshop.com>
2026-07-31 22:54:31 +0800

    fix: wrap page content in .content div and center with max-width
    The typography styles (font-size, line-height, h2-h4, lists, blockquotes,
    tables, code blocks) were scoped to .page-service-content .content but the
    template had no .content wrapper and col-12 was full-width. Add the wrapper
    and constrain the column to col-md-10 col-lg-8 mx-auto for comfortable
    reading measure.

--- innopacks/front/resources/views/pages/show.blade.php
+++ innopacks/front/resources/views/pages/show.blade.php
@@ -12,8 +12,10 @@
     <div class="page-service-content">
       <div class="container">
         <div class="row">
-          <div class="col-12">
-            {!! $page->translation->content ?? '' !!}
+          <div class="col-12 col-md-10 col-lg-8 mx-auto">
+            <div class="content">
+              {!! $page->translation->content ?? '' !!}
+            </div>
           </div>
         </div>
       </div>
