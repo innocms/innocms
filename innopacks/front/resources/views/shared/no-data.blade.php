c16a38cca3fc265d4ce9e247d1971afdd7f98fc1
Edward <edward@innoshop.com>
2026-08-27 16:21:02 +0800

    fix: render no-data svg at intrinsic size and add spacing from breadcrumb

--- innopacks/front/resources/views/shared/no-data.blade.php
+++ innopacks/front/resources/views/shared/no-data.blade.php
@@ -1,4 +1,4 @@
-<div class="d-flex align-items-center flex-column py-3">
-  <img src="{{ asset('images/no-data.svg') }}" class="img-fluid wp-200">
+<div class="d-flex align-items-center flex-column py-5">
+  <img src="{{ asset('images/no-data.svg') }}" class="img-fluid" alt="">
   <span class="fs-4 text-secondary mt-2">{{ $text ?: __('front::common.no_data') }}</span>
 </div>
